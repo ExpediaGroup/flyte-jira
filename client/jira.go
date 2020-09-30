c5f3b68a8377e7ac3df0844b1ef4ec312d409dd7
YToutah993 <ytoutah@expedia.com>
2020-09-30 17:39:37 +0300

    remove not needed log

--- client/jira.go
+++ client/jira.go
@@ -179,7 +179,6 @@ func CreateIssue(project, issueType, title string) (domain.Issue, error) {
 
 	path := "/rest/api/2/issue/"
 	request, err := constructPostRequest(path, string(b))
-	log.Printf("responseeee %v", request.Response)
 	if err != nil {
 		return issue, err
 	}
